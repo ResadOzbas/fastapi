438725c71b509a8aa24628aaf30fc784d7e2f13a
Resad <resad.ozbas@outlook.com>
2024-03-25 17:59:56 +0000

    testing fix for Database_URL not working

--- app/routers/vote.py
+++ app/routers/vote.py
@@ -21,8 +21,12 @@ def vote(vote: schema.Vote, db: Session = Depends(database.get_db), current_user
 
     if (vote.dir == 1):
         if found_vote:
-            raise HTTPException(status_code=status.HTTP_409_CONFLICT, detail=f'user {
-                                current_user.id} has already voted on post with an id of {vote.post_id}')
+            raise HTTPException(status_code=status.HTTP_409_CONFLICT,
+                                detail=(
+                                    f"user "
+                                    f"{current_user.id} has already voted on post "
+                                    f"with an id of {vote.post_id}"
+                                ))
         new_vote = models.Vote(post_id=vote.post_id, user_id=current_user.id)
         db.add(new_vote)
         db.commit()
