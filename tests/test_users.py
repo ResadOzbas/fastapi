70b1563d370dd96b234852d242bdfac2a9e73f15
Resad <resad.ozbas@outlook.com>
2024-03-28 13:23:15 +0000

    created test database and overrid dev db

--- tests/test_users.py
+++ tests/test_users.py
@@ -1,6 +1,38 @@
 from fastapi.testclient import TestClient
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
+from app.database import get_db
 from app.main import app
 from app import schema
+from app.config import settings
+
+# creating a test database
+SQLALCHEMY_DATABASE_URL = (
+    f"postgresql://{settings.database_username}:"
+    f"{settings.database_password}@{settings.database_hostname}:"
+    f"{settings.database_port}/{settings.database_name}_test"
+)
+
+
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
+
+TestingSessionLocal = sessionmaker(
+    autocommit=False, autoflush=False, bind=engine)
+
+Base = declarative_base()
+
+
+def override_get_db():
+    db = TestingSessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+
+# override the get_db dependency with the override_get_db function
+app.dependency_overrides[get_db] = override_get_db
 
 client = TestClient(app)
 
@@ -16,5 +48,5 @@ def test_create_user():
     response = client.post(
         "/users/", json={"email": "hello123@gmail.com", "password": "password123"})
     newUser = schema.UserOut(**response.json())
-    assert response.json().get("email") == "hello123@gmail.com"
+    assert newUser.email == "hello123@gmail.com"
     assert response.status_code == 201
