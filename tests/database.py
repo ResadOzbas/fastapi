b71b0d6bdbea6274aeac0ab8b458c8322a10bb1a
Resad <resad.ozbas@outlook.com>
2024-03-30 15:07:54 +0000

    separated the test database code and testing code and added fixtures

---
+++ tests/database.py
@@ -0,0 +1,54 @@
+from fastapi.testclient import TestClient
+import pytest
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
+from app.database import get_db
+from app.main import app
+from app.config import settings
+from app.database import Base
+
+
+SQLALCHEMY_DATABASE_URL = (
+    f"postgresql://{settings.database_username}:"
+    f"{settings.database_password}@{settings.database_hostname}:"
+    f"{settings.database_port}/{settings.database_name}_test"
+)
+
+# Create a SQLAlchemy engine using the provided database URL
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
+
+# Create a session factory with specific settings
+TestingSessionLocal = sessionmaker(
+    autocommit=False, autoflush=False, bind=engine)
+
+Base.metadata.create_all(bind=engine)
+
+
+@pytest.fixture()
+def session():
+    # drop all tables then create them for testing
+    Base.metadata.drop_all(bind=engine)
+    Base.metadata.create_all(bind=engine)
+    db = TestingSessionLocal()
+    try:
+        # Yield the session to be used as the dependency
+        yield db
+    finally:
+        # Close the session after it's been used
+        db.close()
+
+
+@pytest.fixture()
+def client(session):
+    def override_get_db():
+
+        try:
+            # Yield the session to be used as the dependency
+            yield session
+        finally:
+            # Close the session after it's been used
+            session.close()
+
+    app.dependency_overrides[get_db] = override_get_db
+    yield TestClient(app)
--- tests/test_users.py
+++ tests/test_users.py
@@ -1,52 +1,23 @@
-from fastapi.testclient import TestClient
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
-from sqlalchemy.ext.declarative import declarative_base
-from app.database import get_db
-from app.main import app
 from app import schema
-from app.config import settings
-
-# creating a test database
-SQLALCHEMY_DATABASE_URL = (
-    f"postgresql://{settings.database_username}:"
-    f"{settings.database_password}@{settings.database_hostname}:"
-    f"{settings.database_port}/{settings.database_name}_test"
-)
+from .database import client, session
 
 
-engine = create_engine(SQLALCHEMY_DATABASE_URL)
-
-TestingSessionLocal = sessionmaker(
-    autocommit=False, autoflush=False, bind=engine)
-
-Base = declarative_base()
-
-
-def override_get_db():
-    db = TestingSessionLocal()
-    try:
-        yield db
-    finally:
-        db.close()
-
-
-# override the get_db dependency with the override_get_db function
-app.dependency_overrides[get_db] = override_get_db
-
-client = TestClient(app)
-
-
-def test_root():
+def test_root(client):
+    # Send a GET request to the root endpoint
     response = client.get("/")
+    # Print the message from the response JSON
     print(response.json().get("message"))
+    # Assert that the message is "Welcome to my API!?"
     assert response.json().get("message") == "Welcome to my API!?"
     assert response.status_code == 200
 
 
-def test_create_user():
+def test_create_user(client):
+    # Send a POST request to the "/users/" endpoint with JSON data
     response = client.post(
         "/users/", json={"email": "hello123@gmail.com", "password": "password123"})
+    # Create a UserOut object from the response JSON
     newUser = schema.UserOut(**response.json())
+    # Assert that the email of the new user is "hello123@gmail.com"
     assert newUser.email == "hello123@gmail.com"
     assert response.status_code == 201
