e4658bff29cdae63677ebae30de7fda4389bead1
Resad <resad.ozbas@outlook.com>
2024-04-07 03:44:05 +0100

    created conftest.py and moved fixtures

---
+++ tests/conftest.py
@@ -0,0 +1,68 @@
+from fastapi.testclient import TestClient
+import pytest
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
+from app.database import get_db
+from app.main import app
+from app.config import settings
+from app.database import Base
+
+
+SQLALCHEMY_DATABASE_URL = (
+    f"postgresql://{settings.database_username}:"
+    f"{settings.database_password}@{settings.database_hostname}:"
+    f"{settings.database_port}/{settings.database_name}_test"
+)
+
+# Create a SQLAlchemy engine using the provided database URL
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
+
+# Create a session factory with specific settings
+TestingSessionLocal = sessionmaker(
+    autocommit=False, autoflush=False, bind=engine)
+
+Base.metadata.create_all(bind=engine)
+
+
+@pytest.fixture()
+def session():
+    # drop all tables then create them for testing
+    Base.metadata.drop_all(bind=engine)
+    Base.metadata.create_all(bind=engine)
+    db = TestingSessionLocal()
+    try:
+        # Yield the session to be used as the dependency
+        yield db
+    finally:
+        # Close the session after it's been used
+        db.close()
+
+
+@pytest.fixture()
+def client(session):
+    def override_get_db():
+
+        try:
+            # Yield the session to be used as the dependency
+            yield session
+        finally:
+            # Close the session after it's been used
+            session.close()
+
+    app.dependency_overrides[get_db] = override_get_db
+    yield TestClient(app)
+
+
+@pytest.fixture()
+def test_user(client):
+    user_data = {"email": "hello123@gmail.com", "password": "password123"}
+
+    res = client.post("/users/", json=user_data)
+
+    assert res.status_code == 201
+
+    new_user = res.json()
+    new_user['password'] = user_data['password']
+
+    return new_user
--- tests/test_users.py
+++ tests/test_users.py
@@ -1,20 +1,7 @@
 import pytest
+from jose import jwt
 from app import schema
-from .database import client, session
-
-
-@pytest.fixture()
-def test_user(client):
-    user_data = {"email": "hello123@gmail.com", "password": "password123"}
-
-    res = client.post("/users/", json=user_data)
-
-    assert res.status_code == 201
-
-    new_user = res.json()
-    new_user['password'] = user_data['password']
-
-    return new_user
+from app.config import settings
 
 
 # def test_root(client):
@@ -41,5 +28,11 @@ def test_create_user(client):
 def test_login_user(client, test_user):
     response = client.post(
         "/login", data={"username": test_user['email'], "password": test_user['password']})
+    login_res = schema.Token(**response.json())
+    payload = jwt.decode(login_res.access_token, settings.secret_key,
+                         algorithms=[settings.algorithm])
+    id = payload.get("user_id")
 
+    assert id == test_user['id']
+    assert login_res.token_type == "bearer"
     assert response.status_code == 200
