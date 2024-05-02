e90a3f548b53964f0ecc5cfbc8f417699587867b
Resad <resad.ozbas@outlook.com>
2024-05-02 12:38:12 +0100

    completed get all posts test

--- tests/test_posts.py
+++ tests/test_posts.py
@@ -5,8 +5,19 @@ from app import schema
 
 def test_get_all_posts(authorised_client, test_posts):
     res = authorised_client.get("/posts/")
+    post_data = res.json()
+    # print(post_data)
 
-    for post in res.json():
-        print(post['Post']['title'])
-        # posts = schema.Post(res.json())
+    def validate(post):
+        # Extract the relevant dictionary if the data is nested under 'Post'
+        if 'Post' in post:
+            post = post['Post']
+        return schema.PostResponse(**post)
+
+    # Apply validate to each item in post_data and print the results
+    posts_map = map(validate, post_data)
+
+    print(list(posts_map))
+
+    assert len(post_data) == len(test_posts)
     assert res.status_code == 200
